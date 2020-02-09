3514c80789c7ed638e5434c9f69733c1f0c21ec0
Pasquale <p3dimaria@hotmail.it>
2020-02-09 14:50:32 +0100

    flake: use overlays to add  x-compilation support

--- flake.nix
+++ flake.nix
@@ -3,23 +3,38 @@
 
   outputs = { self, nixpkgs }:
     let
-      system = "x86_64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
+      systems = ["x86_64-darwin"];
+      lib = import ${nixpkgs}/lib;
+      forAllSystems = f: lib.genAttrs systems (system: f (
+        import nixpkgs {
+          inherit system;
+          overlays = [self.overlay];
+        }
+      ));
+
     in {
-      lib.mkHome = configuration:
-        import home-manager/home-manager.nix {
-          inherit pkgs;
-          configuration = configuration // { _module.args.pkgs = pkgs; };
-          nixpkgsSrc = nixpkgs;
+      overlay = self: pkgs: {
+        home-manager = pkgs.callPackage ./home-manager {
+          path = toString ./.;
         };
+        mkHome = configuration:
+          import home-manager/home-manager.nix {
+            inherit pkgs;
+            configuration = configuration // { _module.args.pkgs = pkgs; };
+            nixpkgsSrc = nixpkgs;
+          };
+      };
 
-      packages.${system}.home-manager = pkgs.callPackage ./home-manager { };
+      packages = forAllSystems (pkgs: {inherit (pkgs) home-manager mkHome});
 
-      defaultPackage.${system} = self.packages.${system}.home-manager;
+      defaultPackage = forAllSystems (pkgs: pkgs.home-manager);
 
-      homeConfiguration = self.lib.mkHome {
+      homeConfiguration.exampleOnDarwin = self.packages.x86_64-darwin.mkHome {
         home.homeDirectory = "/home/username";
         home.username = "username";
       };
+      
+      nixosModules.home-manager = import ./nixos;
+      darwinModules.home-manager = import ./nix-darwin;
     };
 }
